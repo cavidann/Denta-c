87f704f761a9ce2eb4d8ecfe9e2f812d5e04c788
cavidann <cavi98@icloud.com>
2017-04-20 00:07:11 -1100

    menuulasts

--- buttons/buttonscript/buttonscript.js
+++ buttons/buttonscript/buttonscript.js
@@ -1,8 +1,63 @@
 $(document).ready(function () {
-   $(".search").click(function(){
-    $(".nav-pills").css({"color":"white"}).html("<h2><b>TYPE AND HIT ENTER</b></h2>");
-    console.log("A")
-   })
+    $(".search").click(function () {
+        $(".nav-pills").css({ "color": "white" }).html("<input placeholder='TYPE AND HIT ENTER'></input>");
+        console.log("A")
+    })
+    $(document).ready(function () {
+        $('#sidebar-btn').click(function () {
+            console.log($('#sidebar').width());
+            $('#sidebar-btn').toggleClass('visible1');
+            $('#sidebar').toggleClass('visible');
+        });
+        var accordion = document.querySelector('#sidebar');
+        accordion.addEventListener('click', function (event) {
+            var item = event.target;
+            if (item.nextElementSibling.style.display == 'block') {
+                $(".pages").hide();
+                $(".galery").hide();
+                $(".blog").hide();
+            } else {
+                $(".pages").hide();
+                $(".galery").hide();
+                $(".blog").hide();
+                item.nextElementSibling.style.display = 'block';
+            }
+        });
+    });
+    $(window).scroll(function () {
 
-   
+        scroll = $(window).scrollTop();
+        console.log(scroll)
+        if (scroll >= 118) {
+            $(".menu").css({
+                "position": "fixed",
+                "top": 0,
+                "left":0,
+                "width": "100vw",
+               "margin": "0 auto"
+            });
+           
+        }else{
+             $(".menu").css({
+                "position": "static",
+                 "width": "inherit"
+            });
+         
+        }
+         if (scroll >= 500) {
+              $(".daire").css({
+                "position": "fixed",
+                "bottom": 0,
+                "right": 10
+            });
+         }else{
+             $(".daire").css({
+                "position": "absolute",
+                "right":"10px",
+                "bottom":"-500px"                
+            });
+         
+        }
+
+    });
 });
